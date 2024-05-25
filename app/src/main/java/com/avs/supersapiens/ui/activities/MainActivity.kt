8321bcdd24d982b39bf839d63797dad9ae5de02f
Alejandro Vialard Santana <a.vialard.developer@gmail.com>
2024-05-25 16:01:52 +0000

    FEAT: Imagen de usuario

--- app/src/main/java/com/avs/supersapiens/ui/activities/MainActivity.kt
+++ app/src/main/java/com/avs/supersapiens/ui/activities/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.avs.supersapiens.ui.activities
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.avs.supersapiens.R
 import com.avs.supersapiens.utils.ProgressManager
-import androidx.appcompat.widget.Toolbar
-import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var progressManager: ProgressManager
+    private lateinit var userIcon: ImageView
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +37,8 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         setupActionBarWithNavController(navController)
 
-        val userIcon: ImageView = findViewById(R.id.userIcon)
+        userIcon = findViewById(R.id.userIcon)
+        progressBar = findViewById(R.id.progressBar)
         userIcon.setOnClickListener {
             if (navController.currentDestination?.id == R.id.homeFragment) {
                 navController.navigate(R.id.action_homeFragment_to_userFragment)
@@ -36,18 +47,61 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Cargar la imagen de perfil
+        loadProfileImage(userIcon)
+
         // Botón para borrar las preferencias
         val clearScoresButton: ImageView = findViewById(R.id.clearScoresButton)
         clearScoresButton.setOnClickListener {
             progressManager.clearAllScores()
-            verifyPreferencesCleared()
         }
     }
 
-    private fun verifyPreferencesCleared() {
-        val score1 = progressManager.getScore("1")
-        val score2 = progressManager.getScore("2")
-        Log.d("MainActivity", "Scores after clearing: gameId 1 = $score1, gameId 2 = $score2")
+    private fun loadProfileImage(userIcon: ImageView) {
+        val sharedPreferences = getSharedPreferences("user_profile", Context.MODE_PRIVATE)
+        val imageString = sharedPreferences.getString("profile_image", null)
+        imageString?.let {
+            progressBar.visibility = View.VISIBLE
+            if (it.startsWith("content://") || it.startsWith("file://")) {
+                val uri = Uri.parse(it)
+                Glide.with(this)
+                    .asBitmap()
+                    .load(uri)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            userIcon.setImageBitmap(resource)
+                            progressBar.visibility = View.GONE
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            progressBar.visibility = View.GONE
+                        }
+                    })
+            } else {
+                val resourceId = it.toInt()
+                Glide.with(this)
+                    .asBitmap()
+                    .load(resourceId)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                            userIcon.setImageBitmap(resource)
+                            progressBar.visibility = View.GONE
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            progressBar.visibility = View.GONE
+                        }
+                    })
+            }
+        }
+    }
+
+    fun updateProfileImage(imageString: String) {
+        val sharedPreferences = getSharedPreferences("user_profile", Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString("profile_image", imageString).apply()
+        loadProfileImage(userIcon)
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/avs/supersapiens/ui/fragments/UserFragment.kt
+++ app/src/main/java/com/avs/supersapiens/ui/fragments/UserFragment.kt
@@ -1,20 +1,34 @@
 package com.avs.supersapiens.ui.fragments
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.avs.supersapiens.R
 import com.avs.supersapiens.databinding.FragmentUserBinding
+import com.avs.supersapiens.ui.activities.MainActivity
+import com.yalantis.ucrop.UCrop
+import java.io.File
+import java.io.InputStream
+import java.util.*
 
 class UserFragment : Fragment() {
 
     private lateinit var binding: FragmentUserBinding
+    private val PICK_IMAGE_REQUEST = 1
+    private val UCROP_REQUEST_CODE = UCrop.REQUEST_CROP
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentUserBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -22,9 +36,93 @@ class UserFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Añadir lógica para cambiar el avatar
+        loadProfileImage()
+
         binding.changeAvatarButton.setOnClickListener {
-            // Lógica para cambiar el avatar
+            pickImageFromGallery()
+        }
+
+        binding.avatarExample1.setOnClickListener {
+            setProfileImageResource(R.drawable.ic_avatar_1)
+        }
+
+        binding.avatarExample2.setOnClickListener {
+            setProfileImageResource(R.drawable.ic_avatar_2)
+        }
+
+        binding.avatarExample3.setOnClickListener {
+            setProfileImageResource(R.drawable.ic_avatar_3)
+        }
+    }
+
+    private fun pickImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                PICK_IMAGE_REQUEST -> {
+                    data?.data?.let { startCrop(it) }
+                }
+                UCROP_REQUEST_CODE -> {
+                    handleCropResult(data)
+                }
+            }
+        }
+    }
+
+    private fun startCrop(uri: Uri) {
+        val destinationUri = Uri.fromFile(File(context?.cacheDir, "cropped_image_${UUID.randomUUID()}"))
+        val options = UCrop.Options()
+        options.setCircleDimmedLayer(true)
+        UCrop.of(uri, destinationUri)
+            .withAspectRatio(1f, 1f)
+            .withMaxResultSize(150, 150)
+            .withOptions(options)
+            .start(requireContext(), this, UCROP_REQUEST_CODE)
+    }
+
+    private fun handleCropResult(data: Intent?) {
+        val resultUri = UCrop.getOutput(data!!)
+        resultUri?.let {
+            setProfileImageUri(it)
+            (activity as? MainActivity)?.updateProfileImage(it.toString())
+        }
+    }
+
+    private fun setProfileImageResource(resourceId: Int) {
+        binding.userAvatar.setImageResource(resourceId)
+        saveProfileImage(resourceId.toString())
+        (activity as? MainActivity)?.updateProfileImage(resourceId.toString())
+    }
+
+    private fun setProfileImageUri(uri: Uri) {
+        val inputStream: InputStream? = context?.contentResolver?.openInputStream(uri)
+        val selectedImage = BitmapFactory.decodeStream(inputStream)
+        binding.userAvatar.setImageBitmap(selectedImage)
+        saveProfileImage(uri.toString())
+        (activity as? MainActivity)?.updateProfileImage(uri.toString())
+    }
+
+    private fun saveProfileImage(imageString: String) {
+        val sharedPreferences = activity?.getSharedPreferences("user_profile", Context.MODE_PRIVATE)
+        sharedPreferences?.edit()?.putString("profile_image", imageString)?.apply()
+    }
+
+    private fun loadProfileImage() {
+        val sharedPreferences = activity?.getSharedPreferences("user_profile", Context.MODE_PRIVATE)
+        val imageString = sharedPreferences?.getString("profile_image", null)
+        imageString?.let {
+            if (it.startsWith("content://") || it.startsWith("file://")) {
+                val uri = Uri.parse(it)
+                setProfileImageUri(uri)
+            } else {
+                val resourceId = it.toInt()
+                setProfileImageResource(resourceId)
+            }
         }
     }
 }
