66d652c70334939d918f494e1cc64a1efb09d3e0
Alejandro Vialard Santana <a.vialard.developer@gmail.com>
2024-05-25 14:37:33 +0000

    FEAT: Confeti y mensaje de compartir

--- app/src/main/java/com/avs/supersapiens/ui/activities/GameResultActivity.kt
+++ app/src/main/java/com/avs/supersapiens/ui/activities/GameResultActivity.kt
@@ -1,9 +1,12 @@
 package com.avs.supersapiens.ui.activities
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.avs.supersapiens.databinding.ActivityGameResultBinding
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class GameResultActivity : AppCompatActivity() {
 
@@ -29,5 +32,44 @@ class GameResultActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
+
+        binding.shareButton.setOnClickListener {
+            shareResults(correctAnswers, totalQuestions)
+        }
+
+        if (correctAnswers == 10) {
+            showConfetti()
+        }
+    }
+
+    private fun shareResults(correctAnswers: Int, totalQuestions: Int) {
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            val message = """
+                ¡He obtenido $correctAnswers de $totalQuestions respuestas correctas en el juego de *SuperSapiens*!
+                
+                ¿Puedes superarme? ¡Descarga la app *SuperSapiens* y juega para mejorar tus habilidades en inglés, matemáticas y más!
+                
+                [Enlace a la app en la tienda]
+            """.trimIndent()
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+        }
+        startActivity(Intent.createChooser(shareIntent, "Compartir resultados a través de"))
+    }
+
+    private fun showConfetti() {
+        val konfettiView = binding.konfettiView
+
+        konfettiView.build()
+            .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+            .setDirection(0.0, 359.0)
+            .setSpeed(1f, 5f)
+            .setFadeOutEnabled(true)
+            .setTimeToLive(2000L)
+            .addShapes(Shape.Square, Shape.Circle)
+            .addSizes(Size(12, 5f))
+            .setPosition(-50f, konfettiView.width + 50f, -50f, -50f)
+            .streamFor(300, 5000L)
     }
 }
